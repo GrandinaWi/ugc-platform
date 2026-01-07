600abf4338e7c3dc09f9f37c32a395bb9881c59b
vadim <vad-gab9@yandex.ru>
2026-01-08 01:43:38 +0500

    feat: open server

--- services/users/cmd/app/main.go
+++ services/users/cmd/app/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"users/internal/app"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	dsn := os.Getenv("USERS_DB_DSN")
+	if dsn == "" {
+		log.Fatal("USERS_DB_DSN is not set")
+	}
+	application, err := app.New(dsn)
+	if err != nil {
+		log.Fatalf("application.New: %s", err)
+	}
+	if err := application.Run(); err != nil {
+		log.Fatalf("application.Run: %s", err)
 	}
 }
