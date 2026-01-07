73490993f68b495424a988b9ad2abe38fd345dfd
vadim <vad-gab9@yandex.ru>
2026-01-08 01:45:59 +0500

    feat: get pack godotenv

--- services/users/cmd/app/main.go
+++ services/users/cmd/app/main.go
@@ -4,9 +4,14 @@ import (
 	"log"
 	"os"
 	"users/internal/app"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 	dsn := os.Getenv("USERS_DB_DSN")
 	if dsn == "" {
 		log.Fatal("USERS_DB_DSN is not set")
--- services/users/go.mod
+++ services/users/go.mod
@@ -2,4 +2,7 @@ module users
 
 go 1.25
 
-require golang.org/x/crypto v0.46.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+)
