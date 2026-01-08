d800153f659bc7a40cfa6a3a6fcf10862087a05e
vadim <vad-gab9@yandex.ru>
2026-01-08 23:30:19 +0500

    feat: write config func that get info in main.go

---
+++ api-gateway/internal/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import "os"
+
+type Config struct {
+	Port      string
+	JWTSecret []byte
+	UsersAPI  string
+}
+
+func Load() *Config {
+	return &Config{
+		Port:      getEnv("PORT", "8080"),
+		JWTSecret: []byte(getEnv("JWT_SECRET", "ugc-sercret")),
+		UsersAPI:  getEnv("USERS_API", "http://users-api:8080"),
+	}
+}
+func getEnv(key, def string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return def
+}
