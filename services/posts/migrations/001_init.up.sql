9d3c8401450a2866ff277ec7fc00a42187da8c63
vadim <vad-gab9@yandex.ru>
2026-01-09 14:10:52 +0500

    feat: change compose where add posts

---
+++ services/posts/migrations/001_init.up.sql
@@ -0,0 +1,11 @@
+-- users_auth: данные для логина
+CREATE TABLE IF NOT EXISTS posts (
+                                     id BIGSERIAL PRIMARY KEY,
+                                     author_id BIGINT NOT NULL,
+                                     title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    like_count INTEGER DEFAULT 0,
+    comment_count INTEGER DEFAULT 0,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+                             );
