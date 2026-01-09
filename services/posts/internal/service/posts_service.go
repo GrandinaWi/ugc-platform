1b47a40bcc72777000f30b7523f16cd9f4764c34
vadim <vad-gab9@yandex.ru>
2026-01-09 12:20:50 +0500

    feat: add interface for posts service

---
+++ services/posts/internal/service/posts_service.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"context"
+	"posts/internal/model"
+)
+
+type Service interface {
+	PostCreate(ctx context.Context, author_id int64, title, content string) (int64, error)
+	PostDelete(ctx context.Context, post_id int64) error
+	PostGet(ctx context.Context, post_id int64) (*model.Post, error)
+	PostsGet(ctx context.Context, post_id int64) ([]*model.Post, error)
+}
