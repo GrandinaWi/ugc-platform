197e922eaa797b2d5c2beebe16b448fcca94c76e
vadim <vad-gab9@yandex.ru>
2026-01-09 11:58:28 +0500

    feat: add posts Repository interface

--- services/posts/internal/repository/posts_repository.go
+++ services/posts/internal/repository/posts_repository.go
@@ -1,1 +1,13 @@
 package repository
+
+import (
+	"context"
+	"posts/internal/model"
+)
+
+type Repository interface {
+	RepoCreate(ctx context.Context, author_id int64, title, content string) (int64, error)
+	RepoGetPosts(ctx context.Context) ([]model.Post, error)
+	RepoGetPost(ctx context.Context, id int64) (*model.Post, error)
+	RepoDelete(ctx context.Context, id int64) error
+}
