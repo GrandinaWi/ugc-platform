35fe28445cfb3ce921dd69af0f3984e516e9b46f
vadim <vad-gab9@yandex.ru>
2026-01-09 11:52:47 +0500

    feat: add model for post

---
+++ services/posts/internal/model/model.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type Post struct {
+	ID           string    `json:"id"`
+	AuthorID     string    `json:"author_id"`
+	Title        string    `json:"title"`
+	Content      string    `json:"content"`
+	LikeCount    int       `json:"like_count"`
+	CommentCount int       `json:"comment_count"`
+	CreatedAt    time.Time `json:"created_at"`
+}
