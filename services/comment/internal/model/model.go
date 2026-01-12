e58a75ed475067c1f8751daa80e9afa7b8266f19
vadim <vad-gab9@yandex.ru>
2026-01-12 20:11:00 +0500

    feat: write model for comments

---
+++ services/comment/internal/model/model.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Comment struct {
+	ID        int64     `json:"id"`
+	PostID    int64     `json:"post_id"`
+	UserID    int64     `json:"user_id"`
+	ParentID  *int64    `json:"parent_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+}
