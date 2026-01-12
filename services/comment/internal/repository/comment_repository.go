f36a57f4b9614431b1eeb21aad483caf805676ae
vadim <vad-gab9@yandex.ru>
2026-01-12 20:22:53 +0500

    feat: add comment interface

---
+++ services/comment/internal/repository/comment_repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"comment/internal/model"
+	"context"
+)
+
+type Repository interface {
+	Create(ctx context.Context, comment *model.Comment) (int64, error)
+	GetByID(ctx context.Context, id int64) (*model.Comment, error)
+	Delete(ctx context.Context, id int64, userId int64) error
+	GetByAll(ctx context.Context, postId int64) ([]*model.Comment, error)
+}
