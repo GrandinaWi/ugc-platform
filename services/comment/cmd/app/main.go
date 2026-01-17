b20f41ede7e91c2b2f33b9451662799c15fa4976
vadim <vad-gab9@yandex.ru>
2026-01-17 13:59:01 +0500

    write main go

--- services/comment/cmd/app/main.go
+++ services/comment/cmd/app/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"comment/internal/app"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+	application, err := app.New(dsn)
+	if err != nil {
+		log.Fatalf("application.New: %s", err)
+	}
+	if err := application.Run(); err != nil {
+		log.Fatalf("application.Run: %s", err)
 	}
 }
